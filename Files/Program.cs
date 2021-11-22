b50b6b0a0fa93c7bac24103a86185fa42ffe0c60
Sergey Zhizhko <11r3start11@gmail.com>
2021-11-22 21:44:31 +0200

    add work with files

---
+++ Files/Program.cs
@@ -0,0 +1,28 @@
+﻿string filePath = "test.txt";
+FileInfo fi = new FileInfo(filePath);
+
+if(!fi.Exists)
+{
+    Console.WriteLine("oops file not exists");
+}
+
+FileStream fs = new FileStream(filePath, FileMode.Open);
+
+int bufferSize = 12;
+
+byte[] buffer = new byte[bufferSize];
+
+
+
+for (int j = 0; j < 3; j++)
+{
+    fs.Seek(bufferSize*j, SeekOrigin.Begin);
+    fs.Read(buffer);
+
+    for (int i = 0; i < buffer.Length; i++)
+    {
+        Console.Write((char)buffer[i]);
+    }
+}
+
+fs.Close();
