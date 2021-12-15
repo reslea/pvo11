b619bf0bfc4be326afa1435ff25f2da7e83a5000
Sergey Zhizhko <11r3start11@gmail.com>
2021-12-15 22:00:37 +0200

    add interfaces sample

---
+++ Interfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿// Дано номер билета (6 чисел)
+// если сумма первой половины билета равна сумме второй
+// то билет счастливый
+
+string ticketNumber = Console.ReadLine()!;
+IHappyTicketChecker checker = null;
+
+bool isHappy = checker.IsHappy(ticketNumber);
+
+if (isHappy)
+{
+    Console.WriteLine("The ticket is happy");
+}
+else
+{
+    Console.WriteLine("Sorry, better luck next time");
+}
+
+public interface IHappyTicketChecker
+{
+    bool IsHappy(string ticketNumber);
+}
+
+public class HappyTicketChecker : IHappyTicketChecker
+{
+    public bool IsHappy(string ticketNumber)
+    {
+        throw new NotImplementedException();
+    }
+}
