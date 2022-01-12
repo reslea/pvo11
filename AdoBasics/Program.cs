ad220f4d569dc8f32accf7a566e7d7710dafb613
Sergey Zhizhko <11r3start11@gmail.com>
2022-01-12 23:24:06 +0200

    add ado net samples

---
+++ AdoBasics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+
+var server = @"(localdb)\MSSQLLocalDB";
+var database = "People";
+
+var connectionString = $"Server={server};Database={database};Trusted_Connection=True;";
+
+using (var connection = new SqlConnection(connectionString))
+{
+    connection.Open();
+
+    var name = Console.ReadLine();
+    var age = 20;
+
+    SqlCommand command = InsertUser(connection, name, age);
+
+    Console.WriteLine(command.CommandText);
+
+    var affectedRows = command.ExecuteNonQuery();
+
+    Console.WriteLine($"affected rows: {affectedRows}");
+}
+
+SqlCommand InsertUser(SqlConnection connection, string name, int age)
+{
+    SqlCommand command = connection.CreateCommand();
+
+    command.CommandText =
+        $"INSERT INTO Users (Name, Age) VALUES (@name, @age)";
+    command.Parameters.AddWithValue("name", name);
+    command.Parameters.AddWithValue("age", age);
+
+    return command;
+}
